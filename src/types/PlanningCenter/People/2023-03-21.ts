5d4d7445fcd1fdcf3f5836bf20584376c753f216
twcrews <twcrews96@gmail.com>
2023-05-11 08:50:05 -0500

    Added optionals to People API types.

--- src/types/PlanningCenter/People/2023-03-21.ts
+++ src/types/PlanningCenter/People/2023-03-21.ts
@@ -48,9 +48,9 @@ export type Campus = {
 };
 
 export type Carrier = {
-  value: string;
-  name: string;
-  international: boolean;
+  value?: string;
+  name?: string;
+  international?: boolean;
 };
 
 export type Condition = {
@@ -76,184 +76,184 @@ export type ConnectedPerson = {
 };
 
 export type Email = {
-  address: string;
-  location: string;
-  primary: boolean;
-  created_at: Date;
-  updated_at: Date;
-  blocked: boolean;
+  address?: string;
+  location?: string;
+  primary?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
+  blocked?: boolean;
 };
 
 export type FieldDatum = {
-  file: {
-    url: string;
+  file?: {
+    url?: string;
   };
-  file_content_type: string;
-  file_name: string;
-  file_size: string;
-  value: string;
+  file_content_type?: string;
+  file_name?: string;
+  file_size?: string;
+  value?: string;
 };
 
 export type FieldDefinition = {
-  data_type: string;
-  name: string;
-  sequence: number;
-  slug: string;
-  config: string;
-  deleted_at: Date;
-  tab_id: number;
+  data_type?: string;
+  name?: string;
+  sequence?: number;
+  slug?: string;
+  config?: string;
+  deleted_at?: Date;
+  tab_id?: number;
 };
 
 export type FieldOption = {
-  value: string;
-  sequence: number;
+  value?: string;
+  sequence?: number;
 };
 
 export type Form = {
-  name: string;
-  description: string;
-  active: boolean;
-  archived_at: Date;
-  created_at: Date;
-  updated_at: Date;
-  deleted_at: Date;
-  submission_count: number;
-  public_url: string;
-  recently_viewed: boolean;
-  archived: boolean;
+  name?: string;
+  description?: string;
+  active?: boolean;
+  archived_at?: Date;
+  created_at?: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+  submission_count?: number;
+  public_url?: string;
+  recently_viewed?: boolean;
+  archived?: boolean;
 };
 
 export type FormField = {
-  field_type: string;
-  label: string;
-  description: string;
-  required: boolean;
-  settings: string;
-  sequence: number;
-  created_at: Date;
-  updated_at: Date;
+  field_type?: string;
+  label?: string;
+  description?: string;
+  required?: boolean;
+  settings?: string;
+  sequence?: number;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type FormFieldOption = {
-  label: string;
-  sequence: number;
-  created_at: Date;
-  updated_at: Date;
+  label?: string;
+  sequence?: number;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type FormSubmission = {
-  verified: boolean;
-  requires_verification: boolean;
-  created_at: Date;
+  verified?: boolean;
+  requires_verification?: boolean;
+  created_at?: Date;
 };
 
 export type FormSubmissionValue = {
-  display_value: string;
-  attachments: any[];
+  display_value?: string;
+  attachments?: any[];
 };
 
 export type Household = {
-  name: string;
-  member_count: number;
-  primary_contact_name: string;
-  created_at: Date;
-  updated_at: Date;
-  avatar: string;
-  primary_contact_id: number;
+  name?: string;
+  member_count?: number;
+  primary_contact_name?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  avatar?: string;
+  primary_contact_id?: number;
 };
 
 export type HouseholdMembership = {
-  person_name: string;
-  pending: boolean;
+  person_name?: string;
+  pending?: boolean;
 };
 
 export type InactiveReason = {
-  value: string;
+  value?: string;
 };
 
 export type List = {
-  name: string;
-  auto_refresh: boolean;
-  status: string;
-  has_inactive_results: boolean;
-  include_inactive: boolean;
-  returns: string;
-  return_original_if_none: boolean;
-  subset: string;
-  automations_active: boolean;
-  automations_count: number;
-  paused_automations_count: number;
-  description: string;
-  invalid: boolean;
-  name_or_description: string;
-  recently_viewed: boolean;
-  refreshed_at: Date;
-  starred: boolean;
-  total_people: number;
-  batch_completed_at: Date;
-  created_at: Date;
-  updated_at: Date;
+  name?: string;
+  auto_refresh?: boolean;
+  status?: string;
+  has_inactive_results?: boolean;
+  include_inactive?: boolean;
+  returns?: string;
+  return_original_if_none?: boolean;
+  subset?: string;
+  automations_active?: boolean;
+  automations_count?: number;
+  paused_automations_count?: number;
+  description?: string;
+  invalid?: boolean;
+  name_or_description?: string;
+  recently_viewed?: boolean;
+  refreshed_at?: Date;
+  starred?: boolean;
+  total_people?: number;
+  batch_completed_at?: Date;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type ListCategory = {
-  name: string;
-  created_at: Date;
-  updated_at: Date;
-  organization_id: number;
+  name?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  organization_id?: number;
 };
 
 export type ListResult = {
-  created_at: Date;
-  updated_at: Date;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type ListStar = {
-  created_at: Date;
+  created_at?: Date;
 };
 
 export type ListShare = {
-  permission: ListSharePermission;
-  group: PermissionGroup;
-  created_at: Date;
-  name: string;
+  permission?: ListSharePermission;
+  group?: PermissionGroup;
+  created_at?: Date;
+  name?: string;
 };
 
 export type MailchimpSyncStatus = {
-  status: string;
-  error: string;
-  progress: number;
-  completed_at: Date;
-  segment_id: number;
+  status?: string;
+  error?: string;
+  progress?: number;
+  completed_at?: Date;
+  segment_id?: number;
 };
 
 export { InactiveReason as MaritalStatus };
 
 export type Message = {
-  kind: MessageKind;
-  to_addresses: string;
-  subject: string;
-  file: string;
-  delivery_status: string;
-  reject_reason: string;
-  created_at: Date;
-  sent_at: Date;
-  bounced_at: Date;
-  rejection_notification_sent_at: Date;
-  from_name: string;
-  from_address: string;
-  read_at: Date;
-  app_name: string;
-  message_type: string;
+  kind?: MessageKind;
+  to_addresses?: string;
+  subject?: string;
+  file?: string;
+  delivery_status?: string;
+  reject_reason?: string;
+  created_at?: Date;
+  sent_at?: Date;
+  bounced_at?: Date;
+  rejection_notification_sent_at?: Date;
+  from_name?: string;
+  from_address?: string;
+  read_at?: Date;
+  app_name?: string;
+  message_type?: string;
 };
 
 export type MessageGroup = {
-  uuid: string;
-  message_type: string;
-  from_address: string;
-  subject: string;
-  message_count: number;
-  system_message: boolean;
-  created_at: Date;
+  uuid?: string;
+  message_type?: string;
+  from_address?: string;
+  subject?: string;
+  message_count?: number;
+  system_message?: boolean;
+  created_at?: Date;
 };
 
 export { InactiveReason as NamePrefix };
@@ -261,245 +261,245 @@ export { InactiveReason as NamePrefix };
 export { InactiveReason as NameSuffix };
 
 export type Note = {
-  note: string;
-  created_at: Date;
-  updated_at: Date;
-  display_date: Date;
-  note_category_id: number;
-  organization_id: number;
-  person_id: number;
-  created_by_id: number;
+  note?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  display_date?: Date;
+  note_category_id?: number;
+  organization_id?: number;
+  person_id?: number;
+  created_by_id?: number;
 };
 
 export type NoteCategory = {
-  name: string;
-  locked: boolean;
-  created_at: Date;
-  updated_at: Date;
-  organization_id: number;
+  name?: string;
+  locked?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
+  organization_id?: number;
 };
 
 export type NoteCategoryShare = {
-  group: PermissionGroup;
-  permission: NoteCategorySharePermission;
-  person_id: number;
+  group?: PermissionGroup;
+  permission?: NoteCategorySharePermission;
+  person_id?: number;
 };
 
 export { ListResult as NoteCategorySubscription };
 
 export type Organization = {
-  name: string;
-  country_code: string;
-  date_format: number;
-  time_zone: string;
-  contact_website: string;
-  created_at: Date;
-  avatar_url: string;
+  name?: string;
+  country_code?: string;
+  date_format?: number;
+  time_zone?: string;
+  contact_website?: string;
+  created_at?: Date;
+  avatar_url?: string;
 };
 
 export type PeopleImport = {
-  attribs: string;
-  status: PeopleImportStatus;
-  created_at: Date;
-  updated_at: Date;
-  processed_at: Date;
-  undone_at: Date;
+  attribs?: string;
+  status?: PeopleImportStatus;
+  created_at?: Date;
+  updated_at?: Date;
+  processed_at?: Date;
+  undone_at?: Date;
 };
 
 export type PeopleImportConflict = {
-  kind: string;
-  name: string;
-  message: string;
-  data: string;
-  conflicting_changes: string;
-  ignore: boolean;
-  created_at: Date;
-  updated_at: Date;
+  kind?: string;
+  name?: string;
+  message?: string;
+  data?: string;
+  conflicting_changes?: string;
+  ignore?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type PeopleImportHistory = {
-  name: string;
-  created_at: Date;
-  updated_at: Date;
-  conflicting_changes: Record<string | number | symbol, any>;
-  kind: string;
+  name?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  conflicting_changes?: Record<string | number | symbol, any>;
+  kind?: string;
 };
 
 export type Person = {
-  given_name: string;
-  first_name: string;
-  nickname: string;
-  middle_name: string;
-  last_name: string;
-  birthdate: Date;
-  anniversary: Date;
-  gender: string;
-  grade: number;
-  child: boolean;
-  graduation_year: number;
-  site_administrator: boolean;
-  accounting_administrator: boolean;
-  people_permissions: string;
-  membership: string;
-  inactivated_at: Date;
-  status: string;
-  medical_notes: string;
-  created_at: Date;
-  updated_at: Date;
-  avatar: string;
-  name: string;
-  demographic_avatar_url: string;
-  directory_status: string;
-  passed_background_check: boolean;
-  can_create_forms: boolean;
-  can_email_lists: boolean;
-  school_type: string;
-  remote_id: number;
+  given_name?: string;
+  first_name?: string;
+  nickname?: string;
+  middle_name?: string;
+  last_name?: string;
+  birthdate?: Date;
+  anniversary?: Date;
+  gender?: string;
+  grade?: number;
+  child?: boolean;
+  graduation_year?: number;
+  site_administrator?: boolean;
+  accounting_administrator?: boolean;
+  people_permissions?: string;
+  membership?: string;
+  inactivated_at?: Date;
+  status?: string;
+  medical_notes?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  avatar?: string;
+  name?: string;
+  demographic_avatar_url?: string;
+  directory_status?: string;
+  passed_background_check?: boolean;
+  can_create_forms?: boolean;
+  can_email_lists?: boolean;
+  school_type?: string;
+  remote_id?: number;
 };
 
 export type PersonApp = {
-  allow_pco_login: boolean;
-  people_permissions: PersonAppPermissions;
+  allow_pco_login?: boolean;
+  people_permissions?: PersonAppPermissions;
 };
 
 export type PersonMerger = {
-  created_at: Date;
-  person_to_keep_id: number;
-  person_to_remove_id: number;
+  created_at?: Date;
+  person_to_keep_id?: number;
+  person_to_remove_id?: number;
 };
 
 export type PhoneNumber = {
-  number: string;
-  carrier: string;
-  location: string;
-  primary: boolean;
-  created_at: Date;
-  updated_at: Date;
-  e164: string;
-  international: string;
-  national: string;
-  country_code: string;
+  number?: string;
+  carrier?: string;
+  location?: string;
+  primary?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
+  e164?: string;
+  international?: string;
+  national?: string;
+  country_code?: string;
 };
 
 export type PlatformNotification = {
-  html: string;
+  html?: string;
 };
 
 export type Report = {
-  name: string;
-  body: string;
-  created_at: Date;
-  updated_at: Date;
+  name?: string;
+  body?: string;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type Rule = {
-  subset: string;
-  created_at: Date;
-  updated_at: Date;
+  subset?: string;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type SchoolOption = {
-  value: string;
-  sequence: number;
-  beginning_grade: string;
-  ending_grade: string;
-  school_types: string[];
+  value?: string;
+  sequence?: number;
+  beginning_grade?: string;
+  ending_grade?: string;
+  school_types?: string[];
 };
 
 export type ServiceTime = {
-  start_time: number;
-  day: DayOfWeek;
-  description: string;
+  start_time?: number;
+  day?: DayOfWeek;
+  description?: string;
 };
 
 export type SocialProfile = {
-  site: string;
-  url: string;
-  verified: boolean;
-  created_at: Date;
-  updated_at: Date;
+  site?: string;
+  url?: string;
+  verified?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export type Tab = {
-  name: string;
-  sequence: number;
-  slug: string;
+  name?: string;
+  sequence?: number;
+  slug?: string;
 };
 
 export type Workflow = {
-  name: string;
-  my_ready_card_count: number;
-  total_ready_card_count: number;
-  completed_card_count: number;
-  total_cards_count: number;
-  total_ready_and_snoozed_card_count: number;
-  created_at: Date;
-  updated_at: Date;
-  deleted_at: Date;
-  campus_id: number;
-  workflow_category_id: number;
-  recently_viewed: boolean;
+  name?: string;
+  my_ready_card_count?: number;
+  total_ready_card_count?: number;
+  completed_card_count?: number;
+  total_cards_count?: number;
+  total_ready_and_snoozed_card_count?: number;
+  created_at?: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+  campus_id?: number;
+  workflow_category_id?: number;
+  recently_viewed?: boolean;
 };
 
 export type WorkflowCard = {
-  snooze_until: Date;
-  overdue: boolean;
-  stage: string;
-  calculated_due_at_in_days_ago: number;
-  sticky_assignment: boolean;
-  created_at: Date;
-  updated_at: Date;
-  completed_at: Date;
-  flagged_for_notification_at: Date;
-  removed_at: Date;
-  moved_to_step_at: Date;
+  snooze_until?: Date;
+  overdue?: boolean;
+  stage?: string;
+  calculated_due_at_in_days_ago?: number;
+  sticky_assignment?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
+  completed_at?: Date;
+  flagged_for_notification_at?: Date;
+  removed_at?: Date;
+  moved_to_step_at?: Date;
 };
 
 export type WorkflowCardActivity = {
-  comment: string;
-  content: string;
-  form_submission_url: string;
-  person_avatar_url: string;
-  person_name: string;
-  reassigned_to_avatar_url: string;
-  reassigned_to_name: string;
-  subject: string;
-  type: string;
-  content_is_html: boolean;
-  created_at: Date;
+  comment?: string;
+  content?: string;
+  form_submission_url?: string;
+  person_avatar_url?: string;
+  person_name?: string;
+  reassigned_to_avatar_url?: string;
+  reassigned_to_name?: string;
+  subject?: string;
+  type?: string;
+  content_is_html?: boolean;
+  created_at?: Date;
 };
 
 export type WorkflowCardNote = {
-  note: string;
-  created_at: Date;
+  note?: string;
+  created_at?: Date;
 };
 
 export type WorkflowCategory = {
-  name: string;
-  created_at: Date;
-  updated_at: Date;
+  name?: string;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export { NoteCategoryShare as WorkflowShare };
 
 export type WorkflowStep = {
-  created_at: Date;
-  updated_at: Date;
-  sequence: number;
-  name: string;
-  description: string;
-  auto_snooze_days: number;
-  auto_snooze_value: number;
-  auto_snooze_interval: WorkflowStepSnoozeInterval;
-  expected_response_time_in_days: number;
-  my_ready_card_count: number;
-  total_ready_card_count: number;
-  default_assignee_id: number;
+  created_at?: Date;
+  updated_at?: Date;
+  sequence?: number;
+  name?: string;
+  description?: string;
+  auto_snooze_days?: number;
+  auto_snooze_value?: number;
+  auto_snooze_interval?: WorkflowStepSnoozeInterval;
+  expected_response_time_in_days?: number;
+  my_ready_card_count?: number;
+  total_ready_card_count?: number;
+  default_assignee_id?: number;
 };
 
 export type WorkflowStepAssigneeSummary = {
-  ready_count: number;
-  snoozed_count: number;
+  ready_count?: number;
+  snoozed_count?: number;
 };
 
 export enum FieldType {
