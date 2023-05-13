8988784cd8c7a72201256c5e88d18486205707d4
twcrews <twcrews96@gmail.com>
2023-05-13 15:53:11 -0500

    Added secret entry validation.

--- src/app/secret-dialog/secret-dialog.component.ts
+++ src/app/secret-dialog/secret-dialog.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-secret-dialog',
@@ -7,13 +15,45 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./secret-dialog.component.less'],
 })
 export class SecretDialogComponent {
-  secretDialog = this.fb.group({
-    secret: [null, Validators.required],
-  });
+  get SecretAuthenticationState(): typeof SecretAuthenticationState {
+    return SecretAuthenticationState;
+  }
 
-  constructor(private fb: FormBuilder) {}
+  secretFormControl = new FormControl('', [secretValidator()]);
+  authenticationState = SecretAuthenticationState.InProgress;
+
+  matcher = new SecretErrorStateMatcher();
 
   onSubmit(): void {
     alert('Thanks!');
   }
+}
+
+export class SecretErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+}
+
+export function secretValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const validLength = (control.value ?? '').length == 64;
+    const validFormat = /[a-fA-F0-9]{64}/.test(control.value);
+    const validationError: ValidationErrors = {
+      error: 'something',
+    };
+    console.log(validationError['error']);
+    return validFormat && validLength
+      ? null
+      : validLength
+      ? { error: 'Secret must be in hexidecimal format' }
+      : { error: 'Secret must be 64 characters' };
+  };
+}
+
+enum SecretAuthenticationState {
+  InProgress,
+  Authenticated,
+  Unauthorized,
+  Error,
 }
