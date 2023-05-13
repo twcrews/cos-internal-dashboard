f12b982a19d745a391e1009bee403fbc6117052a
twcrews <twcrews96@gmail.com>
2023-05-13 09:24:31 -0500

    Initial dialog implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import Configuration from '../assets/app.config.json';
+import { MatDialog } from '@angular/material/dialog';
+import { SecretDialogComponent } from './secret-dialog/secret-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'cos-dashboard';
+
+  secretExists =
+    localStorage.getItem(
+      Configuration.planningCenter.api.auth.secretKey ??
+        'planningCenter-api-secret'
+    ) !== null;
+
+  constructor(public dialog: MatDialog) {
+    if (!this.secretExists) {
+      this.openDialog();
+    }
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(SecretDialogComponent, {
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.secretExists = true;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { WidgetComponent } from './widget/widget.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { SecretDialogComponent } from './secret-dialog/secret-dialog.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, WidgetComponent],
+  declarations: [
+    AppComponent,
+    WidgetComponent,
+    DashboardComponent,
+    SecretDialogComponent,
+  ],
   imports: [
+    MatSlideToggleModule,
     BrowserModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
@@ -17,6 +37,17 @@ import { WidgetComponent } from './widget/widget.component';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
