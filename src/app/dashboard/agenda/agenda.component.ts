1ed62b799f34e63307e41ee94506a9b98d8ddaa5
Tommy Crews <tommy@crews.dev>
2024-05-01 09:42:36 -0500

    Restart calendar animation on refresh.

--- src/app/dashboard/agenda/agenda.component.ts
+++ src/app/dashboard/agenda/agenda.component.ts
@@ -9,11 +9,11 @@ import { EventDay } from './lib/types';
 export class AgendaComponent {
   @Input() days?: EventDay[];
 
-  ngAfterViewInit() {
+  ngAfterViewChecked() {
     const body = document.querySelector('.agenda-container');
     const containers = document.querySelectorAll('.events-container');
     containers.forEach(container => {
-      if (container.clientHeight - 5 > (body?.clientHeight ?? 268)) {
+      if (container.clientHeight - 5 > (body?.clientHeight ?? 268) && !container.classList.contains('animated')) {
         container.classList.add('animated');
       }
     })
